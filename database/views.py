7d58c581d7a4a247237375674ef14e4aafb407e4
Sanjith Kumar <53137451+sanjith-kumar-048@users.noreply.github.com>
2022-01-06 19:54:50 +0530

    Revert "Revert "added user object""
    This reverts commit 849ab1f7dc0aeeb8ab42950d1f6477074d38934e.

--- database/views.py
+++ database/views.py
@@ -1,7 +1,22 @@
 # from django.shortcuts import render
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
 from rest_framework import viewsets
-from . import serializers, models
-# Create your views here.
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
+
+#allow user to see the entire list of obj (feeds) without being authenticated yet.
+#from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+#allow user to see the entire list of obj (feeds) only after being authenticated.
+# from rest_framework.permissions import IsAuthenticated
+
+from . import serializers, models, permissions
+
 
 class DepartmentViewSet(viewsets.ModelViewSet):
     queryset = models.Department.objects.all()
@@ -25,4 +40,23 @@ class TeacherViewSet(viewsets.ModelViewSet):
     permission_classes = []
 
 
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """handle creating and upating user profiles"""
+    serializer_class = serializers.UserProfileSerializer
 
+    #get all objects from db
+    queryset = models.UserProfile.objects.all()
+
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
+
+
+#overwrite the default Login classes to display on Browser
+#so that we can test via Browser
+#normally login module is not visible, now we attached this to View
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication token"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
