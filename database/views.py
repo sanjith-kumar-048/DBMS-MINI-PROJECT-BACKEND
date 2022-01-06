b67ea40b4bbc7378ef1c2728608b4a734f434a9a
Sanjith Kumar <sanjithkumar@pop-os.localdomain>
2022-01-06 11:56:25 +0530

    added user object

--- database/views.py
+++ database/views.py
@@ -1,7 +1,3 @@
-# from django.shortcuts import render
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-# from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
